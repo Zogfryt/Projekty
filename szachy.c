94cb26ad2beb8f8180096b2029c6732f0c10f25c
Zogfryt <72657015+Zogfryt@users.noreply.github.com>
2020-12-01 02:27:10 +0100

    Update szachy.c
    Dużo kodu ale niestety wywala błąd

--- szachy.c
+++ szachy.c
@@ -1,10 +1,194 @@
 #include <stdio.h>
 #define size 8
-#define WYGRANA 100000000
-#define PRZEGRANA -10000000
-int figury[13]={0,1,2,3,4,5,6,7,8,9,10,11,12};
- // 0-wolne, 1-krol, 2-krolowa, 3-goniec, 4-wieza, 5-kon, 6-pion   7-12-piony przeciwnika w tej samej kolejnosci
+#define WYGRANA 1800
+#define PRZEGRANA -1800
+#define PION 6
+#define KON 5
+#define WIEZA 4
+#define GONIEC 3
+#define KROLOWA 2
+#define KROL 1
+#define PUSTE 0
+#define KROL_K 7
+#define KROLOWA_K 8
+#define GONIEC_K 9
+#define WIEZA_K 10
+#define KON_K 11
+#define PION_K 12
 
+int MAX_KIER[]={0,8,8,4,4,8,3,8,8,4,4,8,3};
+int MAX_RANGE[]={0,2,8,8,8,2,2,2,8,8,8,2,2};
+int oc[]={0,WYGRANA,180,60,100,60,20,PRZEGRANA,-180,-60,-100,-60,-20};
+int nx,ny,no,nk;
+
+int WX[13][8]=
+{
+    {0,0,0,0,0,0,0,0},
+    {-1,-1,-1,0,1,1,1,0},
+    {-1,-1,-1,0,1,1,1,0},
+    {-1,-1,1,1},
+    {-1,0,1,0},
+    {-1,-2,-2,-1,1,2,2,1},
+    {-1,-1,-1},
+    {-1,-1,-1,0,1,1,1,0},
+    {-1,-1,-1,0,1,1,1,0},
+    {-1,-1,1,1},
+    {-1,0,1,0},
+    {-1,-2,-2,-1,1,2,2,1},
+    {1,1,1}
+};
+
+int WY[13][8]=
+{
+    {0,0,0,0,0,0,0,0},
+    {-1,0,1,1,1,0,-1},
+    {-1,0,1,1,1,0,-1},
+    {-1,1,1,-1},
+    {0,1,0,-1},
+    {-2,-1,1,2,2,1,-1,-2},
+    {-1,0,1},
+    {-1,0,1,1,1,0,-1},
+    {-1,0,1,1,1,0,-1},
+    {-1,1,1,-1},
+    {0,1,0,-1},
+    {-2,-1,1,2,2,1,-1,-2},
+    {1,0,-1}
+
+};
+
+int HEUR[13][size][size]={
+{
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0},
+   {0,0,0,0,0,0,0,0}
+
+},
+{
+    {-4,-2,-2,-1,-1,-2,-2,-4},
+    {-2,0,0,0,0,0,0,-2},
+    {-2,0,1,1,1,1,0,-2},
+    {-1,0,1,1,1,1,0,-1},
+    {0,0,1,1,1,1,0,-1},
+    {-2,1,1,1,1,1,0,-2},
+    {-2,0,1,0,0,0,0,-2},
+    {-4,-2,-2,-1,-1,-2,-2,-4}
+
+},
+{
+    {-6,-8,-8,-10,-10,-8,-8,-6},
+    {-6,-8,-8,-10,-10,-8,-8,-6},
+    {-6,-8,-8,-10,-10,-8,-8,-6},
+    {-6,-8,-8,-10,-10,-8,-8,-6},
+    {-4,-6,-6,-8,-8,-6,-6,-4},
+    {-2,-4,-4,-4,-4,-4,-4,-2},
+    {4,4,0,0,0,0,4,4},
+    {4,6,2,0,0,2,4,6}
+},
+{
+   {-4,-2,-2,-2,-2,-2,-2,-4},
+   {-2,0,0,0,0,0,0,-2},
+   {-2,0,1,2,2,1,0,-2},
+   {-2,1,1,2,2,1,1,-2},
+   {-2,0,2,2,2,2,0,-2},
+   {-2,2,2,2,2,2,2,-2},
+   {-2,1,0,0,0,0,1,-2},
+   {-4,-2,-2,-2,-2,-2,-2,-4}
+},
+{
+   {0,0,0,0,0,0,0,0},
+   {1,2,2,2,2,2,2,1},
+   {-1,0,0,0,0,0,0,-1},
+   {-1,0,0,0,0,0,0,-1},
+   {-1,0,0,0,0,0,0,-1},
+   {-1,0,0,0,0,0,0,-1},
+   {-1,0,0,0,0,0,0,-1},
+   {0,0,0,1,1,0,0,0}
+},
+{
+   {-10,-8,-6,-6,-6,-6,-8,-10},
+   {-8,-4,0,0,0,0,-4,-8},
+   {-6,0,2,3,3,2,0,-6},
+   {-6,1,3,4,4,3,1,-6},
+   {-6,0,3,4,4,3,0,-6},
+   {-6,1,2,3,3,2,1,-6},
+   {-8,-4,0,1,1,0,-4,-8},
+   {-10,-8,-6,-6,-6,-6,-8,-10},
+},
+{
+   {0,0,0,0,0,0,0,0},
+   {10,10,10,10,10,10,10,10},
+   {2,2,4,6,6,4,2,2},
+   {1,1,2,5,5,2,1,1},
+   {0,0,0,4,4,0,0,0},
+   {1,-1,-2,0,0,-2,-1,1},
+   {1,2,2,-4,-4,2,2,1},
+   {0,0,0,0,0,0,0,0}
+},
+{
+    {4,2,2,1,1,2,2,4},
+    {2,0,-1,0,0,0,0,2},
+    {2,-1,-1,-1,-1,-1,0,2},
+    {0,0,-1,-1,-1,-1,0,1},
+    {1,0,-1,-1,-1,-1,0,1},
+    {2,0,-1,-1,-1,-1,0,2},
+    {2,0,0,0,0,0,0,2},
+    {4,2,2,1,1,2,2,4}
+},
+{
+    {-4,-6,-2,0,0,-2,-4,-6},
+    {-4,-4,0,0,0,0,-4,-4},
+    {2,4,4,4,4,4,4,2},
+    {4,6,6,8,8,6,6,4},
+    {6,8,8,10,10,8,8,6},
+    {6,8,8,10,10,8,8,6},
+    {6,8,8,10,10,8,8,6},
+    {6,8,8,10,10,8,8,6}
+},
+{
+    {4,2,2,2,2,2,2,4},
+    {2,-1,0,0,0,0,-1,2},
+    {2,-2,-2,-2,-2,-2,-2,2},
+    {2,0,-2,-2,-2,-2,0,2},
+    {2,-1,-1,-2,-2,-1,-1,2},
+    {2,0,-1,-2,-2,-1,0,2},
+    {2,0,0,0,0,0,0,2},
+    {4,2,2,2,2,2,2,4}
+},
+{
+   {0,0,0,-1,-1,0,0,0},
+   {1,0,0,0,0,0,0,1},
+   {1,0,0,0,0,0,0,1},
+   {1,0,0,0,0,0,0,1},
+   {1,0,0,0,0,0,0,1},
+   {1,0,0,0,0,0,0,1},
+   {-1,-2,-2,-2,-2,-2,-2,-1},
+   {0,0,0,0,0,0,0,0}
+},
+{
+    {10,8,6,6,6,6,8,10},
+    {8,4,0,-1,-1,0,4,8},
+    {6,-1,-2,-3,-3,-2,-1,6},
+    {6,0,-3,-4,-4,-3,0,6},
+    {6,-1,-3,-4,-4,-3,-1,6},
+    {6,0,-2,-3,-3,-2,0,6},
+    {8,4,0,0,0,0,4,8},
+    {10,8,6,6,6,6,8,10}
+},
+{
+    {0,0,0,0,0,0,0,0},
+    {-1,-2,-2,4,4,-2,-2,-1},
+    {-1,1,2,0,0,2,1,-1},
+    {0,0,0,-4,-4,0,0,0},
+    {-1,-1,-2,-5,-5,-2,-1,-1},
+    {-2,-2,-4,-6,-6,-4,-2,-2},
+    {-10,-10,-10,-10,-10,-10,-10,-10},
+    {0,0,0,0,0,0,0,0}
+},};
 
 void wypisz(int plansza[size][size])
 {
@@ -22,55 +206,150 @@ void wypisz(int plansza[size][size])
     printf("|\n  --------------------------------\n");
     }
 }
-void swap(int plansza[Size][Size], int tx, int ty, int dx, int dy);
+int ocena(int plansza[size][size])
 {
-    int tymcz=plansza[tx][ty];
-    int plansza[tx][ty]=plansza[dx][dy];
-    int plansza[dx][dy]=tymcz;
-}
+    int w=0;
+    for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++)
+        w+=(oc[plansza[i][j]] + HEUR[plansza[i][j]][i][j]);
+    return w;
 
+}
 int komputer(int plansza[size][size],char znak,int glebia)
 {
-    int px_pom,py_pom,_pom,o_pom,px,py,dx,dy,kierunek,odleg³oœæ;
+    int px_pom,py_pom,_pom,o_pom,px,py,dx,dy,kierunek,odleglosc;
     int wynik,wmax,wmin,ruch_fig,bita_fig;
 
-    wynik==ocena(plansza);
-    if(znak=='')
-}
+    wynik=ocena(plansza);
+    printf("%d\n",wynik);
+    if( glebia == 0 || wynik > WYGRANA || wynik < PRZEGRANA) {return wynik;}
 
+    if(znak == 'k')
+    {
+        wmax=100*PRZEGRANA;
+        for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++)
+            if(plansza[i][j]>6 && plansza[i][j]<13)
+                for(kierunek =0; kierunek< MAX_KIER[plansza[i][j]];kierunek++)
+                    for(odleglosc = 0; odleglosc <MAX_RANGE[plansza[i][j]]; odleglosc++)
+                    {
+                        dx=(odleglosc -1)*WX[plansza[i][j]][kierunek];
+                        dy=(odleglosc -1)*WY[plansza[i][j]][kierunek];
+                        if( odleglosc >=2 && plansza[i+dx][j+dy] != PUSTE)
+                            break;
+                        dx=odleglosc*WX[plansza[i][j]][kierunek];
+                        dy=odleglosc*WY[plansza[i][j]][kierunek];
+                        if(i+dx < 8 && i+dx >=0 && j+dy >=0 && j+dy < 8)
+                            if(plansza[i+dx][j+dy]==0 || plansza[i+dx][j+dy] <= 6)
+                            if(plansza[i][j]!=12 || (plansza[i+dx][j+dy] == PUSTE && dy ==0) || (plansza[i+dx][i+dy] != PUSTE && dy !=0))
+                            {
+                                ruch_fig = plansza[i][j];
+                                bita_fig = plansza[i+dx][j+dy];
+                                plansza[i+dx][j+dy] = plansza[px][dx];
+                                plansza[i][j]=PUSTE;
+                                if(plansza[i+dx][j+dy] == 12 && i+dx == 7)
+                                    plansza[i+dx][j+dy]=KROLOWA_K;
+                                wynik=komputer(plansza,'g',glebia -1);
+                                plansza[i+dx][j+dy]=bita_fig;
+                                plansza[i][j]=ruch_fig;
+                                if(wynik > wmax)
+                                {
+                                    wmax=wynik;
+                                    if(glebia == 0)
+                                    {
+                                    nx = i;
+                                    ny = j;
+                                    nk = kierunek;
+                                    no = odleglosc;
+                                    }
+
+                                }
+                            }
+                    }
+                    return wmax;
+    }
+    else
+    {
+        wmin=100*WYGRANA;
+        for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++)
+            if(plansza[i][j]>0 && plansza[i][j]<7)
+                for(kierunek =0; kierunek< MAX_KIER[plansza[i][j]];kierunek++)
+                    for(odleglosc = 0; odleglosc <MAX_RANGE[plansza[i][j]]; odleglosc++)
+                    {
+                        dx=(odleglosc -1)*WX[plansza[i][j]][kierunek];
+                        dy=(odleglosc -1)*WY[plansza[i][j]][kierunek];
+                        if( odleglosc >=2 && plansza[i+dx][j+dy] != PUSTE)
+                            break;
+                        dx=odleglosc*WX[plansza[i][j]][kierunek];
+                        dy=odleglosc*WY[plansza[i][j]][kierunek];
+                        if(i+dx < 8 && i+dx >=0 && j+dy >=0 && j+dy < 8)
+                            if(plansza[i+dx][j+dy]==0 || plansza[i+dx][j+dy] <= 6)
+                            if(plansza[i][j]!=6 || (plansza[i+dx][j+dy] == PUSTE && dy ==0) || (plansza[i+dx][i+dy] != PUSTE && dy !=0))
+                            {
+                                ruch_fig = plansza[i][j];
+                                bita_fig = plansza[i+dx][j+dy];
+                                plansza[i+dx][j+dy] = plansza[px][dx];
+                                plansza[i][j]=PUSTE;
+                                if(plansza[i+dx][j+dy] == 6 && i+dx == 0)
+                                    plansza[i+dx][j+dy]=KROLOWA;
+                                wynik=komputer(plansza,'k',glebia -1);
+                                plansza[i+dx][j+dy]=bita_fig;
+                                plansza[i][j]=ruch_fig;
+                                if(wynik < wmin)
+                                    wmin=wynik;
+                            }
+                    }
+                    return wmin;
+    }
+
+}
 int main()
 {
     int plansza[size][size]=
     {
-        {4,5,3,1,2,3,5,4},
-        {6,6,6,6,6,6,6,6},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
+        {10,11,9,7,8,9,11,10},
         {12,12,12,12,12,12,12,12},
-        {10,11,9,7,8,9,11,10}
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {6,6,6,6,6,6,6,6},
+        {4,5,3,1,2,3,5,4}
     };
     wypisz(plansza);
-    int stan=0,int tx, int ty, int dx, int dy;;
-    while(stan != WYGRANA && stan != PRZEGRANA)
+    int stan=0,tx,ty,dx,dy,tymcz;
+    while(1)
     {
         do
         {
-        printf("podaj pozycje pionka którego chcesz przesun¹c");
+        printf("podaj pozycje pionka ktorego chcesz przesunac\n");
         scanf("%d %d",&tx,&ty);
-        }while(plansza[tx][ty]==0 || x>=8 || x<0 || y>=8 || y<0)
+        }while(plansza[tx][ty]==0 || tx>=8 || tx<0 || ty>=8 || ty<0);
 
         do
         {
-        printf("podaj pozycje pionka którego chcesz przesun¹c");
+        printf("podaj pozycje pionka na ktora chcesz przesunac\n");
         scanf("%d %d",&dx,&dy);
-        }while(plansza[x][y]<7 || dx>=8 || dx<0 || dy>=8 || dy<0)
-
-        swap(plansza,tx,ty,dx,dy);
-
+        }while((plansza[dx][dy]<7 && plansza[dx][dy] != 0) || dx>=8 || dx<0 || dy>=8 || dy<0);
+        plansza[dx][dy]=plansza[tx][ty];
+        plansza[tx][ty]=PUSTE;
+        stan=ocena(plansza);
+        if(stan>WYGRANA || stan < PRZEGRANA)
+            break;
         komputer(plansza,'k',4);
-
+        plansza[nx+no*WX[plansza[nx][ny]][nk]][ny+no*WY[plansza[nx][ny]][nk]]=plansza[nx][ny];
+        plansza[nx][ny]=PUSTE;
+        stan=ocena(plansza);
+        if(stan>WYGRANA || stan < PRZEGRANA)
+            break;
+            wypisz(plansza);
     }
+    if(stan > WYGRANA)
+        printf("AI WON!!");
+    else if(stan < PRZEGRANA)
+        printf("YOU WON!!");
+    else
+    printf("nie dziala");
     return 0;
 }
